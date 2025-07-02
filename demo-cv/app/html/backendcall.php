8ac11bbbff0812260fe2545fdec3d498335fdaa8
Michiel Uitdehaag <michiel@muisit.nl>
2025-07-02 09:15:44 +0200

    Moved public files into html directory to avoid non-public files from being exposed

--- demo-cv/app/html/backendcall.php
+++ demo-cv/app/html/backendcall.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include_once(__DIR__ . '/vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+include_once(__DIR__ . '/../vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
 $adminToken = $_SESSION['token'] ?? '';
